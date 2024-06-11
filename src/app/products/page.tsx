26d2f7477f78670a431fa5003476cdc458582079
JEONGSUJONG <jeon95ujong@gmail.com>
2024-06-11 13:12:35 +0900

    Init

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+export default function ProductList() {
+  return (
+    <>
+      <h1 className="text-3xl">Product List</h1>
+      <div className="p-4">
+        <h2>Product1</h2>
+        <h2>Product2</h2>
+        <h2>Product3</h2>
+      </div>
+    </>
+  );
+}
